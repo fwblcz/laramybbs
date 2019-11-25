f1b6a40b161238aca84177d72db7b8bc559d6074
houdunfuwenbin <1964855183@qq.com>
2019-11-25 16:02:42 +0800

    分类话题列表

--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,5 @@ Route::get('email/resend', 'Auth\VerificationController@resend')->name('verifica
 //Route::patch('/users/{user}', 'UsersController@update')->name('users.update');
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+
+Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
